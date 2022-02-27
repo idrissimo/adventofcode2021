87804f89fed37de078db7f026510465328f8d939
idrissi <idrissimo@yahoo.com>
2022-02-27 21:15:13 +0100

    test success with testing data

--- src/day3.rs
+++ src/day3.rs
@@ -1,21 +1,7 @@
 const INPUT: &str = include_str!("3.txt");
-const LINES: i32 = 1000;
-
-const TEST_INPUT: &str = "00100
-11110
-10110
-10111
-10101
-01111
-00111
-11100
-10000
-11001
-00010
-01010";
 
 pub fn run(){
-    part1(TEST_INPUT);
+   println!("{}", part1(INPUT));
 }
 
 // Retourne un array avec le nombre d apparition du '1' dans chaque colonne + le nombre de lignes.
@@ -30,38 +16,45 @@ fn count(input: &str)->[i32;6]{
                     _ => println!("Ain't special"),
 
                 }
-                            
             }
             acc[5] += 1; //line numbers
             acc
-                        
         })
 }
 
 
-
-pub fn part1(input:&str)->[i32;5]{
+//return a tupl (gamma,epsilon)
+pub fn part1(input:&str)-> i32 {
    
     let count = count(input);
 
     let ones = &count[..5];
     let lines = count[5];
-    let mut gama = [0;5];
+   // let mut gamma = ['0';5];
+    //let mut epsilon = ['0';5];
+    let mut g = String::new();
+    let mut e = String::new();
 
     for (i,v) in ones.iter().enumerate() {
         match v {
-            x if *x > lines/2 => gama[i] = 1,
-            x if *x < lines/2 => gama[i] = 0,
+           // x if *x > lines/2 => { gamma[i] = '1';epsilon[i] = '0';}, 
+           // x if *x < lines/2 => { gamma[i] = '0';epsilon[i] = '1';},
+            x if *x > lines/2 => { g.push('1');e.push('0');}, 
+            x if *x < lines/2 => { g.push('0');e.push('1');},
             _ => println!("test"),
         }
-    }       
+    }  
+
+    dbg!(&g);
     
-    //gama = [1,0,1,1,0,]
 
-    gama
+    let gamma = i32::from_str_radix(&g, 2).unwrap();
+    let epsilon = i32::from_str_radix(&e, 2).unwrap();
+   
 
+    gamma*epsilon
 
-    //(gamma,epsilon,consuption)
+    
 
 
 }
@@ -69,7 +62,7 @@ pub fn part1(input:&str)->[i32;5]{
 
 
 #[test]
-fn test_first_part(){
+fn test_d3_first_part(){
     let input = "00100
 11110
 10110
@@ -83,7 +76,7 @@ fn test_first_part(){
 00010
 01010";
 
-//assert_eq!(198, part1(input));
+assert_eq!(198, part1(input));
 
 
 }
