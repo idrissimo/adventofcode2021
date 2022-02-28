2c6d9ce2d41ef50e81d5d85c644301fabcb8d533
idrissi <idrissimo@yahoo.com>
2022-02-28 21:39:40 +0100

    day3 flowchart

--- src/day3.rs
+++ src/day3.rs
@@ -1,16 +1,4 @@
 const INPUT: &str = include_str!("3.txt");
-const INPUT_TEST: &str = "00100
-11110
-10110
-10111
-10101
-01111
-00111
-11100
-10000
-11001
-00010
-01010";
 
 pub fn run(){
    println!("{}", part1(INPUT));
@@ -35,39 +23,30 @@ fn count(input: &str)->[i32;13]{
 }
 
 
-//return a tupl (gamma,epsilon)
+//return consumption = gamma*epsilon
 pub fn part1(input:&str)-> i32 {
    
-    let count = count(input);
+let count = count(input);
 
-    let ones = &count[..12];
-    let lines = count[12];
-   // let mut gamma = ['0';5];
-    //let mut epsilon = ['0';5];
-    let mut g = String::new();
-    let mut e = String::new();
+let ones = &count[..12];
+let lines = count[12];
+  
+let mut g = String::new();
+let mut e = String::new();
 
-    for (i,v) in ones.iter().enumerate() {
-        match v {
-           // x if *x > lines/2 => { gamma[i] = '1';epsilon[i] = '0';}, 
-           // x if *x < lines/2 => { gamma[i] = '0';epsilon[i] = '1';},
-            x if *x > lines/2 => { g.push('1');e.push('0');}, 
-            x if *x < lines/2 => { g.push('0');e.push('1');},
-            _ => println!("test"),
-        }
-    }  
+for (i,v) in ones.iter().enumerate() {
+    match v {
+        x if *x > lines/2 => { g.push('1');e.push('0');}, 
+        x if *x < lines/2 => { g.push('0');e.push('1');},
+        _ => println!("test"),
+    }
+}  
 
-    dbg!(&g);
-    
-
-    let gamma = i32::from_str_radix(&g, 2).unwrap();
-    let epsilon = i32::from_str_radix(&e, 2).unwrap();
+let gamma = i32::from_str_radix(&g, 2).unwrap();
+let epsilon = i32::from_str_radix(&e, 2).unwrap();
    
 
-    gamma*epsilon
-
-    
-
+gamma*epsilon
 
 }
 
