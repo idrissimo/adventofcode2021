d70dcc12ebc9fe561c3f131bb8ea0ad12e8396a4
idrissi <idrissimo@yahoo.com>
2022-03-27 15:18:37 +0000

    minor: formating code

--- src/day3.rs
+++ src/day3.rs
@@ -1,20 +1,20 @@
 const INPUT: &str = include_str!("3.txt");
 
-pub fn run(){
-   println!("{}", part1(INPUT));
+pub fn run() {
+    println!("{}", part1(INPUT));
 }
 
 // Retourne un array avec le nombre d apparition du '1' dans chaque colonne + le nombre de lignes.
-fn count(input: &str)->[i32;13]{
-    input.trim()
+fn count(input: &str) -> [i32; 13] {
+    input
+        .trim()
         .lines()
-        .map(|line|line.trim().chars().collect::<Vec<char>>())
-        .fold([0;13],|mut acc,x|{
-            for (i,v) in x.iter().enumerate(){
+        .map(|line| line.trim().chars().collect::<Vec<char>>())
+        .fold([0; 13], |mut acc, x| {
+            for (i, v) in x.iter().enumerate() {
                 match v {
                     '1' => acc[i] += 1,
                     _ => println!("Ain't special"),
-
                 }
             }
             acc[12] += 1; //line numbers
@@ -22,38 +22,40 @@ fn count(input: &str)->[i32;13]{
         })
 }
 
-
 //return consumption = gamma*epsilon
-pub fn part1(input:&str)-> i32 {
-   
-let count = count(input);
+pub fn part1(input: &str) -> i32 {
+    let count = count(input);
 
-let ones = &count[..12];
-let lines = count[12];
-  
-let mut g = String::new();
-let mut e = String::new();
+    let ones = &count[..12];
+    let lines = count[12];
 
-for (i,v) in ones.iter().enumerate() {
-    match v {
-        x if *x > lines/2 => { g.push('1');e.push('0');}, 
-        x if *x < lines/2 => { g.push('0');e.push('1');},
-        _ => println!("test"),
+    let mut g = String::new();
+    let mut e = String::new();
+
+    for (i, v) in ones.iter().enumerate() {
+        match v {
+            x if *x > lines / 2 => {
+                g.push('1');
+                e.push('0');
+            }
+            x if *x < lines / 2 => {
+                g.push('0');
+                e.push('1');
+            }
+            _ => println!("test"),
+        }
     }
-}  
 
-let gamma = i32::from_str_radix(&g, 2).unwrap();
-let epsilon = i32::from_str_radix(&e, 2).unwrap();
-   
+    let gamma = i32::from_str_radix(&g, 2).unwrap();
+    let epsilon = i32::from_str_radix(&e, 2).unwrap();
 
-gamma*epsilon
-
+    gamma * epsilon
 }
 
-
+pub fn part2(input: &str) {}
 
 #[test]
-fn test_d3_first_part(){
+fn test_d3_first_part() {
     let input = "00100
 11110
 10110
@@ -67,25 +69,5 @@ fn test_d3_first_part(){
 00010
 01010";
 
-assert_eq!(198, part1(input));
-
-
-}
-fn test_d3_second_part(){
-    let input = "00100
-11110
-10110
-10111
-10101
-01111
-00111
-11100
-10000
-11001
-00010
-01010";
-
-assert_eq!(10111, part1(input));
-
-
+    assert_eq!(198, part1(input));
 }
